16d97169615950fe39e7160120d24eb0f2bf196d
Chris Fuller <chris@cjwfuller.com>
2015-05-03 13:19:38 +0100

    Initial Commit

---
+++ api-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  api-test
+//
+//  Created by Chris Fuller on 30/04/2015.
+//  Copyright (c) 2015 Chris Fuller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
